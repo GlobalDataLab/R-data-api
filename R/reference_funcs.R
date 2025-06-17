368ab6ffa720e136cf71b058489781d76ececa75
Aaron van Geffen <aaron@aaronweb.net>
2025-06-17 16:54:51 +0200

    Add gdl_datasets method for listing available datasets

--- R/reference_funcs.R
+++ R/reference_funcs.R
@@ -85,6 +85,35 @@ gdl_countries <- function(session) {
   return(df)
 }
 
+#' Get dataset list
+#'
+#' Returns a list of datasets available.
+#'
+#' @param session
+#' A valid GDL session object to interface with.
+#'
+#' @returns A data frame containing a list of datasets.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Create a session using your API token (provided by environment here)
+#' session <- gdl_session(Sys.getenv('GDL_API_TOKEN'))
+#' # Request list of datasets
+#' datasets <- gdl_datasets(session)
+#' head(datasets, n=10)
+#' }
+gdl_datasets <- function(session) {
+  if (!is(session, GDLSession)) {
+    stop("Argument must be a GDL Session Object")
+  }
+
+  url <- paste0(GDL_BASEURL, '/', session@dataset, '/api/datasets/?token=', session@token)
+  df <- gdl_request_csv(session, url)
+  return(df)
+}
+
 #' Get region list
 #'
 #' Returns a list of regions available for a particular country.
